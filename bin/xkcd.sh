5b4050df990778ab9347f81b29f5bf395256a38f
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-01-02 13:06:58 +0200

    cleanup

--- bin/xkcd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 1727)
-do
-	if [ ! -f "/work/tmp/xkcd/$i" ];	then
-		wget "https://xkcd.com/$i/" --output-document=/work/tmp/xkcd/$i
-	else
-		echo "$i already exists"
-	fi
-done
