d90e45ae555fe60ef5f665549170616fb9cd1c60
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-01-11 22:53:36 +0200

    optimus gpu temp

---
+++ i3blocks/optimus_temp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+gputemp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+
+echo "GPU: $gputemp °C"
+echo "GPU: $gputemp °C"
+
+if [ $gputemp -gt 70 ]; then
+	echo "#FF0000"
+elif [ $gputemp -gt 60 ]; then
+	echo "#FFFF00"
+else
+	echo "#00FF00"
+fi
+
+exit 0
