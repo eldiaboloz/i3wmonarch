82eeff61659f19fab6c2944e1ab72c3cfe20729f
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-02-01 19:09:49 +0200

    some fixes; partial screnshot; dont change ws in i3bar with mouse scroll

--- scripts/git_pull_svn_rebase_work.sh
+++ scripts/git_pull_svn_rebase_work.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 startdir=`pwd`
 for x in `find /work/www -mindepth 2 -maxdepth 2 -name ".git" -type d`; do
-       	cd $x/../
+    cd $x/../
+	echo "Repo : $(basename $(pwd))"
+
+	gitcb=$(git rev-parse --abbrev-ref HEAD)
+	if [ "$gitcb" != "master" ]; then
+		echo "on brach $gitcb so skipping!!!"
+		continue
+	fi
 	git stash -u
        	git pull origin master
        	git svn fetch
--- scripts/git_push_svn_rebase_work.sh
+++ scripts/git_push_svn_rebase_work.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 startdir=`pwd`
 for x in `find /work/www -mindepth 2 -maxdepth 2 -name ".git" -type d`; do
-       	cd $x/../
+    cd $x/../
 	echo "Repo : $(basename $(pwd))"
+
+	gitcb=$(git rev-parse --abbrev-ref HEAD)
+	if [ "$gitcb" != "master" ]; then
+		echo "on brach $gitcb so skipping!!!"
+		continue
+	fi
+
 	git stash -u
 	git svn rebase && git push origin master
 	git stash pop
