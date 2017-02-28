59191a35c8e3c5b0ac8d3ec33a025c62759be635
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-02-28 22:51:11 +0200

    use phpstorm instead of netbeans;define all vms in .xinitrc

--- scripts/start_programs_at_iliyan-arch-work.sh
+++ scripts/start_programs_at_iliyan-arch-work.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #netbeans-php-nightly >/dev/null 2>&1 &
-netbeans >/dev/null 2>&1 &
+#netbeans >/dev/null 2>&1 &
+phpstorm >/dev/null 2>&1 &
 LC_TIME="C" skype >/dev/null 2>&1 &
+LC_TIME="C" skypeforlinux >/dev/null 2>&1 &
 #viber >/dev/null 2>&1 &
 navicat >/dev/null 2>&1 &
 start_audioplayer >/dev/null 2>&1 &
