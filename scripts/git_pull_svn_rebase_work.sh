a941012963d784dbd69a4ab26c0053fde6d369fe
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-05-17 20:13:14 +0300

    simple way to update only single repo

--- scripts/git_pull_svn_rebase_work.sh
+++ scripts/git_pull_svn_rebase_work.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 myname=$(basename "$0")
 dopush=""
-[[ "$myname" == git_push_* ]] && dopush="y"
+[[ "$myname" == *push_* ]] && dopush="y"
 startdir=$(pwd)
-for x in $(find /work/www -mindepth 2 -maxdepth 2 -name ".git" -type d); do
+if [ $# -eq 0 ]; then
+    repos="$(find /work/www -mindepth 2 -maxdepth 2 -name ".git" -type d)"
+else
+    repos="$@"
+fi
+for x in $repos; do
+    reponame="$(basename "$x")"
+    if [ "$reponame" == "$x" ]; then
+        x="/work/www/$x/.git"
+    fi
     cd $x/../
-	echo "Repo : $(basename $(pwd))"
+    echo "Repo : $(basename $(pwd))"
 
-	gitcb=$(git rev-parse --abbrev-ref HEAD)
-	if [ "$gitcb" != "master" ]; then
-		echo "on brach $gitcb so skipping!!!"
-		continue
-	fi
+    gitcb=$(git rev-parse --abbrev-ref HEAD)
+    if [ "$gitcb" != "master" ]; then
+        echo "on brach $gitcb so skipping!!!"
+        continue
+    fi
 
-	git svn info >/dev/null 2>&1
-	if [ $? -ne 0 ]; then
-		# this is not a git-svn repo
-		git pull origin master
-	else
-		# this is a git-svn repo
-		git stash -u \
-		&& git pull origin master \
-		&& git svn fetch \
-		&& git svn rebase \
-		&& ( ( [ -z "$dopush" ] && git push origin master ) || true  ) \
-		&& git stash pop
-	fi
-	echo ""
+    git svn info >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        # this is not a git-svn repo
+        git pull origin master
+    else
+        # this is a git-svn repo
+        git stash -u \
+            && git pull origin master \
+            && git svn fetch \
+            && git svn rebase \
+            && ( ( [ -z "$dopush" ] && git push origin master ) || true  ) \
+            && git stash pop
+    fi
+    echo ""
 done
 cd $startdir
-[ ! -z "$1" ] && read -n1 -r -p "Press any key..." key
+[[ "$myname" == *float* ]] && read -n1 -r -p "Press any key..." key
