d106261ecc08371baf2eca265ef2f0259e3d6e7c
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-05-18 09:32:42 +0300

    fix: follow symlinks

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 wpdir=${WP_DIR:-"/wallpapers"}
-DISPLAY=:0 feh --bg-scale  "$(find "$wpdir" -type f | shuf | sed -n 1p)"
+DISPLAY=:0 feh --bg-scale  "$(find -L "$wpdir" -type f | shuf | sed -n 1p)"
