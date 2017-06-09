f6a51b94ec491ef86627f5ad64bcadabce132ea3
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-06-09 19:57:22 +0300

    hide current window title; show ram usage

---
+++ i3blocks/ram_usage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+arr=( $(free -m | sed -n 2p) )
+echo "RAM: ${arr[2]}/${arr[1]} MB"
+echo "RAM: ${arr[2]}/${arr[1]} MB"
+
+usage=$(((arr[2]*100)/arr[1]))
+
+if [ $usage -gt 60 ]; then
+	echo "#FF0000"
+elif [ $usage -gt 40 ]; then
+       echo "#FFFF00"
+else
+	echo "#00FF00"
+fi
+
+exit 0
