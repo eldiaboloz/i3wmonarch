632d4b0c1ed8b0d5a95da415153bb9549002a349
iliyan ivanov <iliyan87.ivanov@gmail.com>
2017-08-30 18:15:43 +0300

    mpc favs; miner stats; variable display outputs

--- i3blocks/ram_usage.sh
+++ i3blocks/ram_usage.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 arr=( $(free -m | sed -n 2p) )
-echo "RAM: ${arr[2]}/${arr[1]} MB"
-echo "RAM: ${arr[2]}/${arr[1]} MB"
+#echo "RAM: ${arr[2]}/${arr[1]} MB"
+#echo "RAM: ${arr[2]}/${arr[1]} MB"
 
 usage=$(((arr[2]*100)/arr[1]))
-
+echo "RAM: ${usage}%"
+echo "RAM: ${usage}%"
 if [ $usage -gt 60 ]; then
 	echo "#FF0000"
 elif [ $usage -gt 40 ]; then
