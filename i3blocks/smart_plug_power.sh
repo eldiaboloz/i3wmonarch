a4c2b67686f68e7cc179eff081a1aa0ff373a56b
iliyan ivanov <iliyan87.ivanov@gmail.com>
2019-05-10 17:34:03 +0300

    cleanup; minor changes;
    setup go
    select multiple songs for mpc with fzf --multi
    limit volume 20-50% when using headphones volume knob
    lock/unlock macbook via ssh while locking/unlocking linux
    use /usr/bin/php instead of default php from PATH for i3blocks

--- i3blocks/smart_plug_power.sh
+++ i3blocks/smart_plug_power.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
-
 cd ~/bin
-exec php kasa_tplink_cli_local.php "rig${1}-plug" "rig${1}" i3blockPrint
+exec /usr/bin/php kasa_tplink_cli_local.php "rig${1}-plug" "rig${1}" i3blockPrint
