ff3484b7727d3c4f0dd61277e4843e39c1125569
Iliyan Ivanov <iliyan87.ivanov@gmail.com>
2022-07-09 23:17:16 +0300

    flag which symlinks to be optional for cli env

--- scripts/i3wm/create_symlinks.sh
+++ scripts/i3wm/create_symlinks.sh
@@ -1,54 +1,62 @@
 #!/bin/bash
 # where is project cloned
 reporoot=${1:-"/work/dev/personal/i3wmonarch"}
-dirs=(\
-    "$HOME/dev" \
-    "$HOME/.config/htop" \
-    "$HOME/.config/Code - OSS/User" \
-    "$HOME/.config/i3" \
-    "$HOME/.config/parcellite" \
-    "$HOME/.config/gsimplecal" \
-    "$HOME/.ncmpcpp" \
-    "$HOME/.vim/bundle" \
-    "$HOME/.local/share/rofi" \
+dirs=(
+  "$HOME/dev" ""
+  "$HOME/.config/htop" ""
+  "$HOME/.config/Code - OSS/User" "y"
+  "$HOME/.config/i3" "y"
+  "$HOME/.config/parcellite" "y"
+  "$HOME/.config/gsimplecal" "y"
+  "$HOME/.ncmpcpp" ""
+  "$HOME/.vim/bundle" ""
+  "$HOME/.local/share/rofi" "y"
 )
 
-links=(\
-    "/" "$HOME/dev/i3wmonarch" \
-    "/.Xmodmap" "$HOME/.Xmodmap" \
-    "/.vimrc" "$HOME/.vimrc" \
-    "/.xinitrc" "$HOME/.xinitrc" \
-    "/.xfce4-terminal" "$HOME/.config/xfce4/terminal" \
-    "/.parcelliterc" "$HOME/.config/parcellite/parcelliterc" \
-    "/.gsimplecal_config" "$HOME/.config/gsimplecal/config" \
-    "/.xbindkeysrc" "$HOME/.xbindkeysrc" \
-    "/.htoprc" "$HOME/.config/htop/htoprc" \
-    "/.vscode.settings.json" "$HOME/.config/Code - OSS/User/settings.json" \
-    "/.vscode.keybindings.json" "$HOME/.config/Code - OSS/User/keybindings.json" \
-    "/.tmux.conf" "$HOME/.tmux.conf" \
-    "/.zlogin" "$HOME/.zlogin" \
-    "/.zlogout" "$HOME/.zlogout" \
-    "/.Xresources" "$HOME/.Xresources" \
-    "/.common_zshrc" "$HOME/.zshrc" \
-    "/.common_bashrc" "$HOME/.bashrc" \
-    "/.common_profile" "$HOME/.profile" \
-    "/.ncmpcpp_config" "$HOME/.ncmpcpp/config" \
-    "/github.com/robbyrussell/oh-my-zsh" "$HOME/.oh-my-zsh" \
-    "/rofi/config/" "$HOME/.config/rofi" \
-    "/rofi/themes/" "$HOME/.local/share/rofi/themes" \
-    "/systemd" "$HOME/.config/systemd" \
+links=(
+  "/" "$HOME/dev/i3wmonarch" ""
+  "/.Xmodmap" "$HOME/.Xmodmap" "y"
+  "/.vimrc" "$HOME/.vimrc" ""
+  "/.xinitrc" "$HOME/.xinitrc" "y"
+  "/.xfce4-terminal" "$HOME/.config/xfce4/terminal" "y"
+  "/.parcelliterc" "$HOME/.config/parcellite/parcelliterc" "y"
+  "/.gsimplecal_config" "$HOME/.config/gsimplecal/config" "y"
+  "/.xbindkeysrc" "$HOME/.xbindkeysrc" "y"
+  "/.htoprc" "$HOME/.config/htop/htoprc" ""
+  "/.vscode.settings.json" "$HOME/.config/Code - OSS/User/settings.json" "y"
+  "/.vscode.keybindings.json" "$HOME/.config/Code - OSS/User/keybindings.json" "y"
+  "/.tmux.conf" "$HOME/.tmux.conf" ""
+  "/.zlogin" "$HOME/.zlogin" ""
+  "/.zlogout" "$HOME/.zlogout" ""
+  "/.Xresources" "$HOME/.Xresources" "y"
+  "/.common_zshrc" "$HOME/.zshrc" ""
+  "/.common_bashrc" "$HOME/.bashrc" ""
+  "/.common_profile" "$HOME/.profile" ""
+  "/.ncmpcpp_config" "$HOME/.ncmpcpp/config" ""
+  "/github.com/robbyrussell/oh-my-zsh" "$HOME/.oh-my-zsh" ""
+  "/rofi/config/" "$HOME/.config/rofi" "y"
+  "/rofi/themes/" "$HOME/.local/share/rofi/themes" "y"
+  "/systemd" "$HOME/.config/systemd" "y"
 )
+
+[ -n "${2:-}" ] && dotFilesOnlyFlag="y" || dotFilesOnlyFlag=""
 
 dcnt="${#dirs[@]}"
-for (( i=0; i<$dcnt; i+=1 )); do
+for ((i = 0; i < $dcnt; i += 2)); do
   target="${dirs[$i]}"
+  dotFilesOnlyOpt="${dirs[$i + 1]}"
+  # skip this dir
+  [ -n "${dotFilesOnlyFlag}" ] && [ -n "${dotFilesOnlyOpt}" ] && continue
   mkdir -v -p "$target"
 done
 
 lcnt="${#links[@]}"
-for (( i=0; i<$lcnt; i+=2 )); do
+for ((i = 0; i < $lcnt; i += 3)); do
   source="$reporoot${links[$i]}"
-  target="${links[$i+1]}"
+  target="${links[$i + 1]}"
+  dotFilesOnlyOpt="${links[$i + 2]}"
+  # skip this dir
+  [ -n "${dotFilesOnlyFlag}" ] && [ -n "${dotFilesOnlyOpt}" ] && continue
   if [ -e "$source" ]; then
     if [ ! -e "$target" ]; then
       ln -n -f -v -s "$source" "$target"
