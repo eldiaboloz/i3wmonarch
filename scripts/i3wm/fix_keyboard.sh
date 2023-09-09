921865f7ec83bde4fc94289f50a8bf411dd3da31
Iliyan Ivanov <iliyan87.ivanov@gmail.com>
2023-09-09 08:49:02 +0300

    monitor XMG power

--- scripts/i3wm/fix_keyboard.sh
+++ scripts/i3wm/fix_keyboard.sh
@@ -3,11 +3,11 @@
 set -e
 
 export DISPLAY=:0
-export XAUTHORITY=/home/iliyan/.Xauthority
+export XAUTHORITY=$HOME/.Xauthority
 
 # set keyboard layout
 setxkbmap -layout "us,bg" -model "pc104" -variant "euro,phonetic" -option "grp:shifts_toggle,grp_led:scroll"
-xmodmap /home/iliyan/.Xmodmap
+xmodmap $HOME/.Xmodmap
 
 # disable some inputs
 xinput disable 'Microsoft Microsoft® Nano Transceiver v2.0 Mouse' || true
